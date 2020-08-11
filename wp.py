fc083f5c73b8a8526c83095c6b8cb100b3b76a2d
PatoAnastacio207 <pcnc2013@gmail.com>
2020-08-11 19:57:55 -0300

    Initial commit

---
+++ wp.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://web.whatsapp.com/')
+
+nombre = "Nano Spector"
+msg = "Putarraco **con voz de robot**"
+spam = 1
+
+input("Scan qr")
+
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(nombre))
+
+user.click()
+
+msg_box = driver.find_element_by_class_name('_2FbwG')
+
+
+msg_box.send_keys(msg)
+button = driver.find_element_by_class_name('compose-btn-send')
+button.click()
